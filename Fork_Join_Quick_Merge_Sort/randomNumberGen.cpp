ca798a34def420f6cc10230c8d96edbf1b5e4f71
Joshua T. Miltier <jomi3524@colorado.edu>
2020-09-23 23:52:19 -0600

    add random number generator

---
+++ Fork_Join_Quick_Merge_Sort/randomNumberGen.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <fstream>
+#include <cstdio>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+// generates
+int main() {
+   ofstream file;
+   file.open("sourcefile.txt");
+   srand((unsigned)(0));
+   int size = 10000;
+
+   for (int i = 0; i < size; i++) {
+      int ran_data = (rand() % size+1) + 1; // generates int between 1 - <size>
+      file << ran_data << endl;
+   }
+
+   file.close();
+}
