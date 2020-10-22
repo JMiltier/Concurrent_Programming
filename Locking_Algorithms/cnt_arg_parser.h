53d81b57d205cbef8bc2019b9d4549197c24873c
Joshua T. Miltier <jomi3524@colorado.edu>
2020-10-22 02:04:28 -0600

    arg parser for counter and ticket lock

--- Locking_Algorithms/cnt_arg_parser.h
+++ Locking_Algorithms/cnt_arg_parser.h
@@ -1,65 +1,77 @@
 #ifdef __APPLE__
 
-#ifndef ARG_PARSER
-#define ARG_PARSER
+#ifndef CNT_ARG_PARSER
+#define CNT_ARG_PARSER
 
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <sstream>
 
 using namespace std;
 
 struct arg_params {
-  string inputFile, outputFile, algorithm;
-  size_t NUM_THREADS;
+  string outputFile, bar, lock;
+  size_t NUM_THREADS, NUM_ITERATIONS;
 };
 
 struct arg_params arg_parser(int argc, const char* argv[]){
   struct arg_params args;
 
   /* ↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓ ARG CHECKER ↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓ */
-    if (argc == 2 && string(argv[1]) == "--name") printf("Josh Miltier\n"); // check for --name
-    else if (argc == 8) {} // continue on to execution
-    else { // catch all else
-      printf("\nERROR: Program option parameters are invalid.\n"
-            "       Expected 7 options, but got %i.\n"
-            "       Refer to the README for execution instructions.\n\n", argc-1);
-      exit(-1);
-    }
-    /* ↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑ END ARG CHECK ↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑ */
+  if (argc == 2 && string(argv[1]) == "--name") {
+    // check for --name
+    printf("Josh Miltier\n");
+    exit(0);
+  }
+  else if (argc == 8) {} // continue on to execution
+  else { // catch all else
+    printf("\nERROR: Program option parameters are invalid.\n"
+          "       Expected 7 options, but got %i.\n"
+          "       Refer to the README for execution instructions.\n\n", argc-1);
+    exit(-1);
+  }
+  /* ↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑ END ARG CHECK ↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑ */
 
-    /* ↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓ ARG PARSER ↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓ */
-    if (string(argv[2]).rfind("-o", 0)) {
-      printf("ERROR: incorrect flag for output file.   Expected: '-o'  Actual: '%s'\n", argv[2]); exit(-1);
-    } else args.outputFile = string(argv[3]);
+  /* ↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓ ARG PARSER ↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓ */
+  // number of threads (argv 2), with syntax checking
+  if (!string(argv[1]).rfind("-t", 0)) {
+    args.NUM_THREADS = atoi( argv[2] );
+    if (args.NUM_THREADS > 150) { printf("ERROR; too many threads\n"); exit(-1); }
+  } else {
+    printf("ERROR: incorrect flag for number of threads.   Expected: '-t'  Actual: '%s'\n", argv[1]); exit(-1);
+  }
 
-    // txt input file (argv 1)
-    args.inputFile = string(argv[1]);
+  // number of iterations (argv 3), with syntax checking
+  if (string(argv[3]).rfind("-i", 0)) {
+    printf("ERROR: incorrect flag for output file.   Expected: '-i'  Actual: '%s'\n", argv[2]); exit(-1);
+  } else {
+    size_t pos = string(argv[3]).find("=")+1;
+    string num = string(argv[3]).substr(pos);
+    stringstream str2int(num);
+    str2int >> args.NUM_ITERATIONS;
+  }
+  // bar type <sense> or <pthread> (argv 4)
+  if (string(argv[4]) == "--bar=sense") args.bar = "sense";
+  else if (string(argv[4]) == "--bar=pthread") args.bar = "pthread";
+  else { printf("ERROR: algorithm does not exist. Try 'sense' or 'pthread'\n"); exit(-1); }
 
-    // txt output file (argv 3), with syntax checking
-    if (string(argv[2]).rfind("-o", 0)) {
-      printf("ERROR: incorrect flag for output file.   Expected: '-o'  Actual: '%s'\n", argv[2]); exit(-1);
-    } else args.outputFile = string(argv[3]);
+  // lock type <tas, ttas, ticket, mcs, pthread> (argv 5)
+  if (string(argv[5]) == "--lock=tas") args.lock = "tas";
+  else if (string(argv[5]) == "--lock=ttas") args.lock = "ttas";
+  else if (string(argv[5]) == "--lock=ticket") args.lock = "ticket";
+  else if (string(argv[5]) == "--lock=pthread") args.lock = "pthread";
+  else { printf("ERROR: algorithm does not exist. Try 'tas', 'ttas', 'ticket', or 'pthread'\n"); exit(-1); }
 
-    // number of threads (argv 5), with syntax checking
-    if (!string(argv[4]).rfind("-t", 0)) {
-      args.NUM_THREADS = atoi( argv[5] );
-      if (args.NUM_THREADS > 150) { printf("ERROR; too many threads\n"); exit(-1); }
-    } else {
-      printf("ERROR: incorrect flag for number of threads.   Expected: '-t'  Actual: '%s'\n", argv[4]); exit(-1);
-    }
+  // txt output file (argv 7), with syntax checking
+  if (string(argv[6]).rfind("-o", 0)) {
+    printf("ERROR: incorrect flag for output file.   Expected: '-o'  Actual: '%s'\n", argv[6]); exit(-1);
+  } else args.outputFile = string(argv[7]);
 
-    // algorithm type <fjmerge> or <lkbucket> (argv 6)
-    // fork/join merge sort
-    if (string(argv[6]) == "--alg=fjmerge") args.algorithm = "fjmerge";
-    // lock bucket sort
-    else if (string(argv[6]) == "--alg=lkbucket") args.algorithm = "lkbucket";
-    // algorithm not available
-    else { printf("ERROR: algorithm does not exist. Try 'fjmerge' or 'lkbucket'\n"); exit(-1); }
-    /* ↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑ END ARG PARSER ↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑ */
+/* ↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑ END ARG PARSER ↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑ */
 
-    return args;
+  return args;
 }
 
-#endif // ARG_PARSER
+#endif // CNT_ARG_PARSER
 #endif // __APPLE__
--- Locking_Algorithms/counter.cpp
+++ Locking_Algorithms/counter.cpp
@@ -17,13 +17,17 @@
 #include <time.h>         // for timing execution (timespec), clock_t, clock()
 #include "pthread_add.h"  // for when running on macOS
 #include "lk_bucketsort.h"
-#include "arg_parser.h"   // help with parsing data
+#include "cnt_arg_parser.h"   // help with parsing data
 
 using namespace std;
 
 /* initialization of variables */
 int NUM_THREADS = 0;
-int NUM_OPERATIONS = 0;
+int NUM_ITERATIONS = 0;
+int cntr = 0;
+int numberOver = 0;
+pthread_barrier_t bar;
+pthread_mutex_t mutexLock;
 
 /* struct for timer , tv_sec and tv_nsec */
 struct timespec time_start, time_end;
@@ -34,19 +38,61 @@ void time_display(timespec ts, timespec te) {
   // unsigned long, plan on never running out (over 5 centuries)
   unsigned long time_spent = (te.tv_sec - ts.tv_sec)*1e9 +
                              (te.tv_nsec - ts.tv_nsec);
-  printf("Time elapsed is %lu nanoseconds.\n", time_spent);
-  printf("                %f seconds.\n", time_spent/1e9);
+  printf("Time elapsed is %lu nanoseconds\n", time_spent);
+  printf("                %f seconds\n", time_spent/1e9);
 }
 
+
+/* Main Function */
 int main(int argc, const char* argv[]){
 	struct arg_params args_parsed = arg_parser(argc, argv);
-	string inputFile = args_parsed.inputFile;
+	string bar = args_parsed.bar;
 	string outputFile = args_parsed.outputFile;
-	string algorithm = args_parsed.algorithm;
+	string lock = args_parsed.lock;
 	NUM_THREADS = args_parsed.NUM_THREADS;
+  NUM_ITERATIONS = args_parsed.NUM_ITERATIONS;
 
+  // printf("All the data we want:\n%i\n%i\n%s\n%s\n%s\n", NUM_THREADS, NUM_ITERATIONS, bar.c_str(), lock.c_str(), outputFile.c_str());
+
+
+  // execution time
   clock_gettime(CLOCK_REALTIME, &time_start);
-  sleep(3);
+  /*!! do something cool */
   clock_gettime(CLOCK_REALTIME, &time_end);
   time_display(time_start, time_end);
 }
+
+/* *************** BAR FUNCTIONS *************** */
+// sense
+// pthread
+
+/* *************** LOCK FUNCTIONS *************** */
+// tas
+// ttas
+// ticket
+atomic_int next_num (0), serving (0), atomicTID (0);
+
+void ticket_lock() {
+  int num = atomic_fetch_add(&next_num, 1);
+  while(serving.load() != num) {
+    if (cntr > NUM_ITERATIONS) {
+      numberOver = 1;
+      break;
+    }
+  }
+}
+
+void ticket_unlock() {
+  atomic_fetch_add(&serving, 1);
+}
+
+void *counter_ticket_lock(void *filler) {
+  ticket_lock();
+  while(cntr < NUM_ITERATIONS) {
+    if (!numberOver) cntr++;
+    ticket_unlock();
+    ticket_lock();
+  }
+  ticket_unlock();
+}
+// pthread
