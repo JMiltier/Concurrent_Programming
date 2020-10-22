b84d8ab61f367bfe1595bc972b40eedf28e55fe7
Joshua T. Miltier <jomi3524@colorado.edu>
2020-10-22 02:27:36 -0600

    bars and locks added

--- Locking_Algorithms/counter.cpp
+++ Locking_Algorithms/counter.cpp
@@ -21,29 +21,41 @@
 
 using namespace std;
 
-/* initialization of variables */
+/* initialization */
 int NUM_THREADS = 0;
 int NUM_ITERATIONS = 0;
 int cntr = 0;
 int numberOver = 0;
+bool flagOut = false;
 pthread_barrier_t bar;
 pthread_mutex_t mutexLock;
+atomic_int next_num (0), serving (0), atomicTID (0);
+atomic_int sense (0), cnt (0);
+atomic_bool tas_flag (false);
 
-/* struct for timer , tv_sec and tv_nsec */
+/* execution time struct and function */
 struct timespec time_start, time_end;
+void time_display(timespec ts, timespec te);
 
-/* time output calculation and display */
-void time_display(timespec ts, timespec te) {
-  // unsigned int 4,294,967,295, which is only 4.3 seconds
-  // unsigned long, plan on never running out (over 5 centuries)
-  unsigned long time_spent = (te.tv_sec - ts.tv_sec)*1e9 +
-                             (te.tv_nsec - ts.tv_nsec);
-  printf("Time elapsed is %lu nanoseconds\n", time_spent);
-  printf("                %f seconds\n", time_spent/1e9);
-}
+/* bar functions */
+void sense_wait();
+void *counter_sense(void *filler);
+void *counter_bar_pthread(void *filler);
+
+/* lock functions */
+bool tas();
+void tas_lock();
+void tas_unlock();
+void *counter_TSL(void *filler);
+void ttas_lock();
+void *counter_TTSL(void *filler);
+void ticket_lock();
+void ticket_unlock();
+void *counter_ticket_lock(void *filler);
+void *counter_lock_pthread(void *filler);
 
 
-/* Main Function */
+/* *************** Main Function *************** */
 int main(int argc, const char* argv[]){
 	struct arg_params args_parsed = arg_parser(argc, argv);
 	string bar = args_parsed.bar;
@@ -54,7 +66,6 @@ int main(int argc, const char* argv[]){
 
   // printf("All the data we want:\n%i\n%i\n%s\n%s\n%s\n", NUM_THREADS, NUM_ITERATIONS, bar.c_str(), lock.c_str(), outputFile.c_str());
 
-
   // execution time
   clock_gettime(CLOCK_REALTIME, &time_start);
   /*!! do something cool */
@@ -62,16 +73,91 @@ int main(int argc, const char* argv[]){
   time_display(time_start, time_end);
 }
 
+
+/* *************** time output calculation and display *************** */
+void time_display(timespec ts, timespec te) {
+  // unsigned int 4,294,967,295, which is only 4.3 seconds
+  // unsigned long, plan on never running out (over 5 centuries)
+  unsigned long time_spent = (te.tv_sec - ts.tv_sec)*1e9 +
+                             (te.tv_nsec - ts.tv_nsec);
+  printf("Time elapsed is %lu nanoseconds\n", time_spent);
+  printf("                %f seconds\n", time_spent/1e9);
+}
+
+
 /* *************** BAR FUNCTIONS *************** */
-// sense
-// pthread
+/* ****** sense ****** */
+void sense_wait() {
+  int n = NUM_THREADS;
+
+  thread_local bool cur_sense = 0;
+  cur_sense = !cur_sense;
+  int cnt_copy = atomic_fetch_add(&cnt, 1);
+  if (cnt_copy == n - 1) {
+    cnt.store(0, memory_order_relaxed);
+    sense.store(cur_sense);
+  } else while(sense.load() != cur_sense);
+}
+
+void *counter_sense(void *filler) {
+  int tid = atomicTID++;
+  while (cntr < NUM_ITERATIONS) {
+    cntr++;
+    sense_wait();
+  }
+}
+
+/* ****** pthread ****** */
+void *counter_bar_pthread(void *filler) {
+  int tid = atomicTID++;
+  pthread_barrier_wait(&bar);
+  for (int i = 0; i < NUM_ITERATIONS; i++) {
+    if (i % NUM_THREADS == tid) cntr++;
+    pthread_barrier_wait(&bar);
+  }
+}
 
 /* *************** LOCK FUNCTIONS *************** */
-// tas
-// ttas
-// ticket
-atomic_int next_num (0), serving (0), atomicTID (0);
+/* ****** tas ****** */
+bool tas() {
+  if (tas_flag == false) tas_flag = true;
+  return tas_flag;
+}
 
+void tas_lock() {
+  while(!tas());
+}
+
+void tas_unlock() {
+  tas_flag.store(false, memory_order_seq_cst);
+}
+
+void *counter_TSL(void *filler) {
+  tas_lock();
+  while(cntr < NUM_ITERATIONS) {
+    cntr++;
+    tas_unlock();
+    tas_lock();
+  }
+  tas_unlock();
+}
+
+/* ****** ttas ****** */
+void ttas_lock() {
+  while(tas_flag.load() || !tas());
+}
+
+void *counter_TTSL(void *filler) {
+  ttas_lock();
+  while(cntr < NUM_ITERATIONS) {
+    cntr++;
+    tas_unlock();
+    ttas_lock();
+  }
+  tas_unlock();
+}
+
+/* ****** ticket ****** */
 void ticket_lock() {
   int num = atomic_fetch_add(&next_num, 1);
   while(serving.load() != num) {
@@ -95,4 +181,16 @@ void *counter_ticket_lock(void *filler) {
   }
   ticket_unlock();
 }
-// pthread
+
+/* ****** pthread ****** */
+void *counter_lock_pthread(void *filler) {
+  int tid = atomicTID++;
+  while (cntr < NUM_ITERATIONS) {
+    while (cntr % (NUM_THREADS - 1) != tid) {
+      if (cntr >= NUM_ITERATIONS) break;
+    }
+    pthread_mutex_lock(&mutexLock);
+    cntr++;
+    pthread_mutex_unlock(&mutexLock);
+  }
+}
