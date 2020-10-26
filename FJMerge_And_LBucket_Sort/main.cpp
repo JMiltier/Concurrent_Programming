b0bc4177d80fe7875575d45a8b787a9e39dde507
Joshua T. Miltier <jomi3524@colorado.edu>
2020-10-26 11:51:53 -0600

    update folder name

--- FJMerge_And_LBucket_Sort/main.cpp
+++ FJMerge_And_LBucket_Sort/main.cpp
@@ -136,3 +136,65 @@ int main(int argc, const char* argv[]){
 	double elapsed_s = ((double)elapsed_ns)/1000000000.0;
 	printf("Elapsed (s): %lf\n",elapsed_s);
 }
+
+/* ↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓ MERGE SORT ↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓ */
+void merge(int *arr, int low, int high, int mid) {
+	int l_size = mid - low + 1, r_size = high - mid, i, j, k;
+	int* left = new int[l_size];
+	int* right = new int[r_size];
+
+	for (i = 0; i < l_size; i++)
+		left[i] = arr[i + low];
+
+	for (i = 0; i < r_size; i++)
+		right[i] = arr[i + mid + 1];
+
+	i = j = 0; k = low;
+
+	while(i < l_size && j < r_size){
+		if (left[i] <= right[j]) arr[k++] = left[i++];
+		else arr[k++] = right[j++];
+	}
+
+	while (i < l_size) arr[k++] = left[i++];
+	while (j < r_size) arr[k++] = right[j++];
+}
+
+void mergeSort(int *arr, int low, int high) {
+    //divide the array at mid
+		int mid = low + (high - low) / 2;
+    if (low < high){
+        // sort independently using merge sort
+        mid=(low+high)/2;
+        mergeSort(arr, low, mid);
+        mergeSort(arr, mid + 1, high);
+        //merge or conquer sorted arrays
+        merge(arr, low, high, mid);
+    }
+}
+/* ↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑ END MERGE SORT ↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑ */
+
+/* ↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓ BUCKET SORT ↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓ */
+// Function to sort arr[] of size n using bucket sort
+void lk_bucketSort(int *arr, int n) {
+	int i, j, count = 0, max_value = 0;
+
+	// find max key value in arr
+	for (i = 0; i < n; ++i)
+		if (arr[i] > max_value) max_value = arr[i];
+
+	// create n empty buckets
+	auto buckets = vector<unsigned >(static_cast<unsigned int>(max_value + 1));
+
+	// put array elements in different buckets
+	for (i = 0; i < n; ++i)
+		buckets[arr[i]]++;
+
+	// concatenate all buckets into arr[]
+	for (int i = 0; i < buckets.capacity(); ++i)
+		for (int j = 0; j < buckets[i]; ++j) {
+				arr[count] = i;
+				count++;
+		}
+}
+/* ↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑ END BUCKET SORT ↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑ */
